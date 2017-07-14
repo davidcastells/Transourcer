6fc5af786808f9f3cc7de6dd2d4d3d1d380769de
David Castells-Rufas <david.castells@uab.es>
2017-07-14 09:09:45 +0200

    method to get previous instruction

--- src/cat/uab/cephis/analysis/ExecutionBranches.java
+++ src/cat/uab/cephis/analysis/ExecutionBranches.java
@@ -17,6 +17,10 @@
 package cat.uab.cephis.analysis;
 
 import cat.uab.cephis.ast.AST;
+import cat.uab.cephis.ast.ArithmeticExpression;
+import cat.uab.cephis.ast.AssignmentExpression;
+import cat.uab.cephis.ast.ExpressionBlock;
+import cat.uab.cephis.ast.VariableDefinition;
 
 /**
  *
@@ -48,6 +52,35 @@ public class ExecutionBranches
     private static boolean isLastChild(AST child, AST parent)
     {
         return parent.get(parent.size()-1) == child;
+    }
+
+    /**
+     * The previous instruction is determined by checking the parent 
+     * executability as a single instruction/block,
+     * if a control instruction is found in the upwards path we return null
+     * @param node
+     * @return 
+     */
+    public static AST getPreviousInstruction(AST node)
+    {
+        return Hierarchy.getPreviousSibling(getTopOfInstruction(node));        
+    }
+    
+    public static AST getTopOfInstruction(AST node)
+    {
+        AST parent = node.getParent();
+        AST ret = null;
+        
+        if (parent instanceof ExpressionBlock)
+            return getTopOfInstruction(parent);
+        if (parent instanceof ArithmeticExpression)
+            return getTopOfInstruction(parent);
+        if (parent instanceof AssignmentExpression)
+            ret = parent;
+        if (parent instanceof VariableDefinition)
+            ret = parent;
+        
+        return ret;
     }
     
 }
